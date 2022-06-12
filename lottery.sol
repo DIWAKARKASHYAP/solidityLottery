5f594aaa42936d38a8bac6b5bb5fa71585fb0a89
DIWAKARKASHYAP <94478026+DIWAKARKASHYAP@users.noreply.github.com>
2022-06-12 12:31:38 +0530

    Create lottery.sol

---
+++ lottery.sol
@@ -0,0 +1,96 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;  
+
+contract lottery{
+
+    address public deployerAddress = msg.sender ;
+    
+    uint randomNumber ;
+  
+        // deployerAddress ;
+    
+
+    address payable[] public addressOfBuyer;
+
+        
+
+
+    receive() external payable{
+        
+        require( msg.value == 2000000000000000000,"sorry yor transaction value is not equal to 2");
+    
+        addressOfBuyer.push(payable(msg.sender));
+
+}
+
+function totalBalance() public  view returns(uint){
+
+    return address(this).balance;
+
+}
+
+
+function findWinner( ) public {
+
+
+require( msg.sender == deployerAddress , "you are not owner"); //718623
+
+   
+
+        if(addressOfBuyer.length>1){
+            
+            
+
+            randomNumber =  uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,addressOfBuyer.length))) % addressOfBuyer.length;
+
+         address payable winner;
+
+         winner = addressOfBuyer[randomNumber];
+         winner.transfer(totalBalance());
+      
+
+          
+
+
+        
+
+        }
+        else{
+            // return "insufficient person";
+        }
+
+
+}
+
+
+// function payWinner() public payable{
+//     getter.transfer( address(this).balance);
+// }
+
+function changeOwner(address _newOwner) public returns(address){
+
+    require(msg.sender == deployerAddress);
+
+    deployerAddress = _newOwner;
+
+    return deployerAddress ;
+
+}
+
+function removeAllUser() public {
+
+    require(msg.sender == deployerAddress , "SORRY you are not owner");
+
+    while(addressOfBuyer.length > 0) {
+    addressOfBuyer.pop();
+    
+    
+    }
+
+    
+
+}
+
+
+}
